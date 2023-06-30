9e64f8d72a1d1c6d0dcfb2284294c1330f97a66f
Côme Chevallier <come.chevallier@icloud.com>
2023-06-30 15:14:55 +0200

    install supabase for uploading images

---
+++ utils/supabase.upload.ts
@@ -0,0 +1,32 @@
+import { createClient } from "@supabase/supabase-js";
+
+// Create Supabase client
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+);
+
+// Upload file using standard upload
+export async function uploadFile(file: File) {
+  const { data, error } = await supabase.storage
+    .from("parrot_app")
+    .upload(`/uploads/${file.name}`, file);
+  if (error) {
+    console.log(error);
+    return { success: false, data: error.message };
+  } else {
+    return { success: true, data: data.path };
+  }
+}
+
+export async function deleteFile(path: string) {
+  const { data, error } = await supabase.storage
+    .from("parrot_app")
+    .remove([path]);
+  if (error) {
+    console.log(error);
+    return { success: false, data: error.message };
+  } else {
+    return { success: true, data: "Supprimé avec succès" };
+  }
+}
