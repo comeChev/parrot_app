ecfc82f04070c900fa597f9a12397c22a9bcf05c
Côme Chevallier <come.chevallier@icloud.com>
2023-07-15 10:30:45 +0200

    adding cookies consent (with react-cookie-consent)

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -4,17 +4,20 @@ import { getHours } from "@/lib/hours";
 import SiteFooter from "@/components/layout/site/Site.footer";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/utils/nextAuth/nextAuth.options";
+import UiCookieConsent from "@/components/ui/Ui.cookie.consent";
 
 type SiteLayoutProps = { children: ReactNode };
 
 export default async function SiteLayout({ children }: SiteLayoutProps) {
   const hours = await getHours();
   const session = await getServerSession(authOptions);
+
   return (
     <>
       <SiteNav hours={hours} session={session} />
       <main className="flex-1">{children}</main>
       <SiteFooter hours={hours} session={session} />
+      <UiCookieConsent />
     </>
   );
 }
---
+++ components/ui/Ui.cookie.consent.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import React from "react";
+import CookieConsent, { getCookieConsentValue } from "react-cookie-consent";
+import cookies from "@/assets/cookies.png";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function UiCookieConsent() {
+  const cookieConsentValue = getCookieConsentValue();
+
+  return (
+    !cookieConsentValue && (
+      <CookieConsent
+        disableStyles={true}
+        enableDeclineButton={true}
+        buttonClasses="bg-neutral-800 hover:bg-neutral-100 hover:text-neutral-800 text-white font-light border border-neutral-800 py-2 px-4 rounded mr-4 transition-all duration-400 ease-in-out"
+        declineButtonClasses="hover:underline text-neutral-100 font-light mr-4 transition-all duration-400 ease-in-out"
+        containerClasses="sticky bottom-0 bg-neutral-700 bg-opacity-90 text-white text-center p-4 w-full"
+        buttonWrapperClasses="flex items-center w-full justify-end px-4"
+        overlayClasses="fixed top-0 inset-0 bg-neutral-800 bg-opacity-50 z-50 w-full h-full flex flex-col justify-end"
+        overlay={true}
+        buttonText="J'accepte"
+        declineButtonText="Je refuse"
+        debug={true}
+      >
+        <div className="text-start p-4">
+          <div className="flex items-center mb-4">
+            <Image
+              src={cookies}
+              alt="cookies"
+              width={50}
+              height={50}
+              className="mr-2"
+            />
+            <p className="text-lg font-bold text-neutral-100">
+              Ce site utilise des cookies
+            </p>
+          </div>
+          <p className="text-sm mb-1">
+            Nous utilisons des cookies pour nous aider à personnaliser le
+            contenu et surtout pour vous permettre d'avoir la meilleure
+            expérience possible.
+          </p>
+          <p className="text-sm mb-1 hidden md:flex">
+            En cliquant sur{" "}
+            <span className="italic font-medium">"J'accepte"</span>, vous
+            acceptez l'utilisation de ces cookies, comme définie dans notre
+            politique de confidentialité.
+          </p>
+          <Link
+            href="/privacy"
+            className="text-xs hover:underline underline-offset-4"
+          >
+            Lire notre politique de confidentialité
+          </Link>
+        </div>
+      </CookieConsent>
+    )
+  );
+}
